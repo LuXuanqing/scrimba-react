ac8f9797ceb017372983c0118a90aab359f79cf0
陆玄青 <lu.xuanqing@outlook.com>
2023-12-27 01:43:11 +0800

    styling with classes

--- info-site/index.js
+++ info-site/index.js
@@ -1,7 +1,12 @@
 function Header() {
     return <header>
-        <nav>
-            <img src="./react-logo.png" width="40px"></img >
+        <nav className="nav">
+            <img src="./react-logo.png" className="logo"></img >
+            <ul className="nav-items">
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     </header>
 }
