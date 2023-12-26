0adeb60f6a91027fb417cfba254a7cb17facd7d5
陆玄青 <lu.xuanqing@outlook.com>
2023-12-27 01:23:31 +0800

    custom components part 2

--- info-site/index.js
+++ info-site/index.js
@@ -10,15 +10,22 @@
 // }
 function MyComponent() {
     return <div>
-        <img src="./react-logo.png" width="40px"></img >
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px"></img >
+            </nav>
+        </header>
         <h1>Fun facts about React</h1>
-        <ul>
+        <ol>
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 100k stars on Github</li>
             <li>Is maintained by Facebook</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        </ol>
+        <footer>
+            <small>© 2023 Lu Xuanqing development. All rights reserved.</small>
+        </footer>
     </div>
 }
 
