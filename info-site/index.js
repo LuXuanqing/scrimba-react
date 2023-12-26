8acf126702ddf7914230852dab8e3a419fa42678
陆玄青 <lu.xuanqing@outlook.com>
2023-12-27 01:27:53 +0800

    parent/child components

--- info-site/index.js
+++ info-site/index.js
@@ -1,20 +1,19 @@
-// function Navbar() {
-//     return <nav>
-//         <h1>indie developer</h1>
-//         <ul>
-//             <li>Pricing</li>
-//             <li>About</li>
-//             <li>Contact</li>
-//         </ul>
-//     </nav>
-// }
-function MyComponent() {
+function Header() {
+    return <header>
+        <nav>
+            <img src="./react-logo.png" width="40px"></img >
+        </nav>
+    </header>
+}
+
+function Footer() {
+    return <footer>
+        <small>© 2023 Lu Xuanqing development. All rights reserved.</small>
+    </footer>
+}
+
+function MainContent() {
     return <div>
-        <header>
-            <nav>
-                <img src="./react-logo.png" width="40px"></img >
-            </nav>
-        </header>
         <h1>Fun facts about React</h1>
         <ol>
             <li>Was first released in 2013</li>
@@ -23,11 +22,16 @@ function MyComponent() {
             <li>Is maintained by Facebook</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ol>
-        <footer>
-            <small>© 2023 Lu Xuanqing development. All rights reserved.</small>
-        </footer>
+    </div>
+}
+
+function Page() {
+    return <div>
+        <Header />
+        <MainContent />
+        <Footer />
     </div>
 }
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<MyComponent />)
+root.render(<Page />)
