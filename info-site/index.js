5b17890f21f35f03f01598085d9edcab241f7718
陆玄青 <lu.xuanqing@outlook.com>
2023-12-27 00:50:27 +0800

    project part 1

--- info-site/index.js
+++ info-site/index.js
@@ -1,15 +1,26 @@
-function Navbar() {
-    return <nav>
-        <h1>indie developer</h1>
+// function Navbar() {
+//     return <nav>
+//         <h1>indie developer</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// }
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px"></img >
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
-}
-
-ReactDOM.render(
-    <Navbar />,
-    document.querySelector('#root')
+    </div>
 )
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(page)
