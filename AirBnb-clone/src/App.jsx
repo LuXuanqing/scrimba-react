4b7e188584af72610f4ac325883f8118f9957625
luxuanqing <lu.xuanqing@outlook.com>
2023-12-28 22:21:14 +0800

    create hero

--- AirBnb-clone/src/App.jsx
+++ AirBnb-clone/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Hero from "./Hero";
 
 function App() {
   return (
     <div id="app">
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ AirBnb-clone/src/Hero.jsx
@@ -0,0 +1,17 @@
+import heroImage from "./assets/hero.png";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <img src={heroImage} alt="hero image" className="hero-image" />
+      <h1 className="hero-title">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
