a2ce261e50bc14294f92d78be09166534ad2d7b1
luxuanqing <lu.xuanqing@outlook.com>
2023-12-28 22:58:50 +0800

    add A11Y

--- AirBnb-clone/src/Card.jsx
+++ AirBnb-clone/src/Card.jsx
@@ -6,15 +6,15 @@ import dot from "./assets/dot.svg";
 export default function Card() {
   return (
     <div className="card">
-      <img src={image} alt="life lesson with xxx" className="card-image" />
+      <img src={image} alt="main card image" className="card-image" />
       <p className="rating">
-        <img src={star} alt="rating stars" />
+        <img src={star} alt="star icon" />
         <span>5.0</span>
         <span className="light-text-color">(6)</span>
-        <img src={dot} alt="dot seperator" />
+        <img src={dot} alt="dot icon" />
         <span className="light-text-color">USA</span>
       </p>
-      <p>Life lessons with Katie Zaferes</p>
+      <h2 className="card-title">Life lessons with Katie Zaferes</h2>
       <p>
         <span className="heavy-font">From $136</span> / person
       </p>
